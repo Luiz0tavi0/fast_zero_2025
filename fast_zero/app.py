81e042762aee46612928f4ccbb0dbb49506c85d1
luiz_loon <luiz_loon@yahoo.com.br>
2025-05-28 22:03:23 -0300

    schemas e html_response

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,8 +1,33 @@
+from http import HTTPStatus
+
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+from fast_zero.schemas import Message
 
 app = FastAPI()
 
 
-@app.get('/')
+@app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
     return {'message': 'Hello, World!'}
+
+
+@app.get(
+    '/html-response', status_code=HTTPStatus.OK, response_class=HTMLResponse
+)
+def get_html():
+    return """
+    <html>
+        <body>
+            <p class="enfase">Olá mundo em HTML</p>
+        </body>
+        <style>
+        .enfase{
+            color:red;
+            font-weight:bolder;
+            text-decoration-line: underline;
+        }
+        </style>
+    </html>
+"""
